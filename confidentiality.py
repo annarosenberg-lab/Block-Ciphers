97868ed71aa4561a89fbf271c71ef558b217d71d
Tanya Ravichandran <59846633+taanyaar@users.noreply.github.com>
2023-01-24 14:55:38 -0800

    THINGS TO WORK ON IN DESCRIPTION
    - in submit(), append cipher text to one long cipher text after each iteration, currently only producing one at a time and won't let you concatenate them
    - simple decryption, use the built In AES decryption and get it to work rather than implementing
    - create a loop to check for admin= in string and return true if found so slayOrnoSlay is true which is bad cause it should never be true with our encoding
    - add the byte flipping, changing the URL encoding
    - checking if we even did URL encoding correctly lol...

--- confidentiality.py
+++ confidentiality.py
@@ -3,6 +3,7 @@ import sys
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
 
 BLOCKSIZE = 16
 
@@ -15,11 +16,11 @@ def submit(userData, key, iv):
     userData = "userid=456;userdata=" + userData   #pre append userid=456; userdata=
     userData = userData + ";session-id31337"       #append ;session-id31337
     print("\nUSER DATA:\n", userData, "\n")
-    
-    cipherInst = AES.new(key, AES.MODE_ECB)
+
     paddedText = pad(userData.encode(), BLOCKSIZE)          #pad the final string
     print("PADDED TEXT:\n", paddedText)
     
+    cipherText = ""
     for i in range(0, len(paddedText), BLOCKSIZE):
         curTxt = paddedText[i:i+BLOCKSIZE]
         if(i == 0):
@@ -27,18 +28,36 @@ def submit(userData, key, iv):
         else:
             XOR_text = byte_xor(curTxt, cipherText) 
         cipherText = cipherInst.encrypt(XOR_text)
+        print("\nCIPHER TEXT:\n", cipherText, "\n")
         
-    print("\nCIPHER TEXT:\n", cipherText, "\n")
+        #cipherText = cipherText.append(cipherInst.encrypt(XOR_text))
+        
     
     
-
+    slayORnoSlay = verify(cipherText, key, iv)
     
     
-#def verify(decryptStr, key, iv): 
+def verify(cipherText, key, iv): 
+    plaintext = unpad(verifyCipherInst.decrypt(cipherText), BLOCKSIZE)
+    print(plaintext)
+    '''plaintext = ""
+    for i in range(0, len(cipherText), BLOCKSIZE):
+        curCipherTxt = cipherText[i: i+BLOCKSIZE]
+        curCipherTxt_Decrypt = cipherInst.decrypt(curCipherTxt)
+        if(i == 0):
+            XOR_text = byte_xor(curCipherTxt_Decrypt, iv)
+        else:
+            XOR_text = byte_xor(curCipherTxt_Decrypt, lastCipher) 
+        lastCipher = curCipherTxt
+        plaintext = plaintext + XOR_text'''
+    
+            
     
     
     
 key = get_random_bytes(16)
 iv = get_random_bytes(16) 
+cipherInst = AES.new(key, AES.MODE_ECB)
+verifyCipherInst = AES.new(key, AES.MODE_CBC, iv)
 userData = input("What is your string? ")
 submit(userData, key, iv)
