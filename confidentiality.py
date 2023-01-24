79e2163588d51bf7700a2e6573153e33f1393ab2
Tanya Ravichandran <59846633+taanyaar@users.noreply.github.com>
2023-01-24 14:36:36 -0800

    Update confidentiality.py

--- confidentiality.py
+++ confidentiality.py
@@ -4,14 +4,41 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
 
+BLOCKSIZE = 16
 
-submit(userStr, key, iv):
+def byte_xor(ba1, ba2):
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
+def submit(userData, key, iv):
+    userData = userData.replace(';', '%3B')
+    userData = userData.replace('=', '%3D')    
+    userData = "userid=456;userdata=" + userData   #pre append userid=456; userdata=
+    userData = userData + ";session-id31337"       #append ;session-id31337
+    print("\nUSER DATA:\n", userData, "\n")
+    
+    cipherInst = AES.new(key, AES.MODE_ECB)
+    paddedText = pad(userData.encode(), BLOCKSIZE)          #pad the final string
+    print("PADDED TEXT:\n", paddedText)
+    
+    for i in range(0, len(paddedText), BLOCKSIZE):
+        curTxt = paddedText[i:i+BLOCKSIZE]
+        if(i == 0):
+            XOR_text = byte_xor(curTxt, iv)    
+        else:
+            XOR_text = byte_xor(curTxt, cipherText) 
+        cipherText = cipherInst.encrypt(XOR_text)
+        
+    print("\nCIPHER TEXT:\n", cipherText, "\n")
     
     
-verify(decryptStr, key, iv):
+
+    
+    
+#def verify(decryptStr, key, iv): 
+    
     
     
 key = get_random_bytes(16)
 iv = get_random_bytes(16) 
-userInput = input("What is your string?")
-submit(userInput)
+userData = input("What is your string? ")
+submit(userData, key, iv)
