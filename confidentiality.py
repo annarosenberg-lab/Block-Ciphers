2729c5248fea57426e3e6ab3456fefa269b9ac08
Tanya Ravichandran <59846633+taanyaar@users.noreply.github.com>
2023-01-24 13:36:48 -0800

    Add files via upload

---
+++ confidentiality.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad
+
+
+submit(userStr):
+    
+    
+verify(decryptStr):
+    
+    
+key = get_random_bytes(16)
+iv = get_random_bytes(16) 
+userInput = input("What is your string?")
+submit(userInput)
