abdedc4fedeaeb0d54c55596d488f88487a1af1e
Tanya Ravichandran <59846633+taanyaar@users.noreply.github.com>
2023-01-23 23:52:13 -0800

    Update block_cipher.py

--- block_cipher.py
+++ block_cipher.py
@@ -47,8 +47,9 @@ def encrypt(file, key, iv):
             XOR_text = byte_xor(curTxt, iv)    
         else:
             XOR_text = byte_xor(curTxt, ciphertext_cbc) 
+        #print("CUR = ", curTxt)
         ciphertext_cbc = cipher_cbc.encrypt(XOR_text)
-        f_cbc.write(iv)
+        f_cbc.write(ciphertext_cbc)
      
     f.close()
     f_cbc.close()
