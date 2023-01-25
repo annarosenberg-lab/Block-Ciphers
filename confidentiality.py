801041ff9c9c4b1251d19b0b500341243b43d895
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-01-24 17:57:22 -0800

    Update confidentiality.py

--- confidentiality.py
+++ confidentiality.py
@@ -5,6 +5,7 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
 from Crypto.Util.Padding import unpad
 
+
 BLOCKSIZE = 16
 
 def byte_xor(ba1, ba2):
@@ -20,39 +21,20 @@ def submit(userData, key, iv):
     paddedText = pad(userData.encode(), BLOCKSIZE)          #pad the final string
     print("PADDED TEXT:\n", paddedText)
     
-    cipherText = ""
+    cipherText = bytearray()
     for i in range(0, len(paddedText), BLOCKSIZE):
         curTxt = paddedText[i:i+BLOCKSIZE]
-        if(i == 0):
-            XOR_text = byte_xor(curTxt, iv)    
-        else:
-            XOR_text = byte_xor(curTxt, cipherText) 
-        cipherText = cipherInst.encrypt(XOR_text)
+        XOR_text = byte_xor(curTxt, iv) 
+        cipherText.extend(cipherInst.encrypt(XOR_text))
+        iv = cipherInst.encrypt(XOR_text)
         print("\nCIPHER TEXT:\n", cipherText, "\n")
-        
-        #cipherText = cipherText.append(cipherInst.encrypt(XOR_text))
-        
+
+
     
     
-    slayORnoSlay = verify(cipherText, key, iv)
-    
-    
-def verify(cipherText, key, iv): 
-    plaintext = unpad(verifyCipherInst.decrypt(cipherText), BLOCKSIZE)
+def verify(cipherText, key, iv):
+    plaintext = verifyCipherInst.decrypt(cipherText)
     print(plaintext)
-    '''plaintext = ""
-    for i in range(0, len(cipherText), BLOCKSIZE):
-        curCipherTxt = cipherText[i: i+BLOCKSIZE]
-        curCipherTxt_Decrypt = cipherInst.decrypt(curCipherTxt)
-        if(i == 0):
-            XOR_text = byte_xor(curCipherTxt_Decrypt, iv)
-        else:
-            XOR_text = byte_xor(curCipherTxt_Decrypt, lastCipher) 
-        lastCipher = curCipherTxt
-        plaintext = plaintext + XOR_text'''
-    
-            
-    
     
     
 key = get_random_bytes(16)
@@ -61,3 +43,4 @@ cipherInst = AES.new(key, AES.MODE_ECB)
 verifyCipherInst = AES.new(key, AES.MODE_CBC, iv)
 userData = input("What is your string? ")
 submit(userData, key, iv)
+#verify(userData, key, iv)
