56d028cf103e52dc1d9c8182e8b0a4d9142c0e01
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-01-28 14:27:29 -0800

    Update confidentiality.py
    Fixed Byte flipping

--- confidentiality.py
+++ confidentiality.py
@@ -7,8 +7,10 @@ from Crypto.Util.Padding import unpad
 
 BLOCKSIZE = 16
 
-def byte_xor(b1, b2):
-    return bytes([_a ^ _b for _a, _b in zip(b1, b2)])
+def byte_xor(ba1, ba2):
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
+
 
 def submit(userData, key, iv):
     userData = userData.replace(';', '%3B')
@@ -20,46 +22,44 @@ def submit(userData, key, iv):
     paddedText = pad(userData.encode(), BLOCKSIZE)          #pad the final string
     
     cipherText = bytearray()
+    byteFlip = input("Should we byte flip?(Y/N) ")
     for i in range(0, len(paddedText), BLOCKSIZE):
         curTxt = paddedText[i:i+BLOCKSIZE]
         XOR_text = byte_xor(curTxt, iv) 
-        cipherText.extend(cipherInst.encrypt(XOR_text))
+        encryptedBlock = cipherInst.encrypt(XOR_text)
+
+        #byte flipping block 0
+        if byteFlip == "Y":
+            if i ==0:
+                encryptedBlock = flipByte(encryptedBlock, paddedText)
+                
+        cipherText.extend(encryptedBlock)
         iv = cipherInst.encrypt(XOR_text)
     
-    print("\nCIPHER TEXT:\n", cipherText, "\n")         
+    print("\nCIPHER TEXT:\n", cipherText, "\n")        
+
     
-    AdminFound = verify(cipherText, key, iv)
-    if AdminFound:
-        print("\nOH NOOOO ADMIN FOUND")
+    slayORnoSlay = verify(cipherText, key, iv)
+    if slayORnoSlay:
+        print("\nADMIN FOUND")
     else:
-        print("\nYEAHHHHH\n")
+        print("\nNO ADMIN FOUND")
     
     
 def verify(cipherText, key, iv): 
-    byteFlip = input("Should we byte flip?(Y/N) ")
-    if byteFlip == "Y":
-        cipherText = flipByte(cipherText)
-    #print("\ntype: /n", cipherText.decode('utf-8'))
-    
     plaintext = verifyCipherInst.decrypt(cipherText)
-    print("\nPLAIN DECRYPTED TEXT:\n", plaintext)
-    
     plaintext = unpad(plaintext, BLOCKSIZE)
-    
-    #print("\nBYTE CHECK = \n", str(plaintext)[21])
-    if "admin=true" in str(plaintext):
+    print("PLAIN DECRYPTED TEXT:\n", plaintext)
+    if ";admin=true" in str(plaintext):
         return True
     return False
 
-def flipByte(cipherText):
-    changeBlock = cipherText[15:32]
-    temp = bytes(";admin=true.....",'utf-8' )
-    cipherTextTemp = cipherText
-    block = bytes(b ^ a for a, b in zip(changeBlock, temp))
-    cipherText = cipherTextTemp[:15] + block + cipherTextTemp[32:]
-    
-    print("\nNEW CIPHER TEXT: \n", cipherText, "\n")
-    return cipherText
+def flipByte(cipherText, paddedText):
+    changeBlock = cipherText[0:16]
+    desiredText = bytes(";admin=true.....",'utf-8' )
+    x = byte_xor(desiredText, paddedText[16:32])
+    flippedBlock = byte_xor(changeBlock, x)
+    return flippedBlock
 
        
     
