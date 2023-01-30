6d20155e0a54b7d033abf9591d6a93f8a45aa34f
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-01-30 14:28:32 -0800

    Update Asgn3_Task3_PartA.py

--- Asgn3_Task3_PartA.py
+++ Asgn3_Task3_PartA.py
@@ -1,10 +1,11 @@
 from Crypto.Util import number
-primeLen = 5
+
+#Key generation
+primeLen = 200
 p = number.getPrime(primeLen)
 q = number.getPrime(primeLen)
 while p == q:
     q = number.getPrime(primeLen)
-
 n = p * q
 phi_n = (p-1) * (q-1)
 e = 65537
@@ -12,3 +13,18 @@ d = pow(e, -1, phi_n)
 
 publicKey = [e, n]
 privateKey = [d, n]
+
+#encryption 
+plaintext = "hello"
+print("plaintext: ", plaintext)
+plaintext = plaintext.encode('utf-8').hex()
+plaintext = int(plaintext, 16)
+ciphertext = pow(plaintext, e, n)
+print("ciphertext:", ciphertext)
+
+#decryption
+decryptedtext = pow(ciphertext, d, n)
+decryptedtext = hex(decryptedtext)[2:]
+decryptedtext = bytearray.fromhex(decryptedtext)
+decryptedtext = str(decryptedtext, 'utf-8')
+print("decryptedtext:", decryptedtext)
